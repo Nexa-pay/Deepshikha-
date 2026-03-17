0610209f4762cfc7b6e2b7c1437fa57c42b001b4
Nexa-pay <stellargamingg99@gmail.com>
2026-03-18 02:46:29 +0530

    Update web.py

--- web.py
+++ web.py
@@ -1,1 +1,29 @@
+from flask import Flask, request, jsonify
+from payments import handle_payment
+from database import users, payments
 
+app = Flask(__name__)
+
+
+@app.route("/webhook", methods=["POST"])
+def webhook():
+    data = request.json
+
+    if data["event"] == "payment_link.paid":
+        handle_payment(data)
+
+    return "OK"
+
+
+@app.route("/dashboard")
+def dashboard():
+    total_users = users.count_documents({})
+    premium = users.count_documents({"premium": True})
+
+    total = sum(p["amount"] for p in payments.find()) / 100
+
+    return jsonify({
+        "users": total_users,
+        "premium": premium,
+        "earnings": total
+    })
