0d5e1c482e6f3d97e16d000699f96b9dae872994
Nexa-pay <stellargamingg99@gmail.com>
2026-03-18 02:44:55 +0530

    Update database.py

--- database.py
+++ database.py
@@ -1,1 +1,10 @@
+from pymongo import MongoClient
+from config import MONGO_URI
 
+client = MongoClient(MONGO_URI)
+db = client["telegram_bot"]
+
+users = db["users"]
+payments = db["payments"]
+coupons = db["coupons"]
+chats = db["chats"]
