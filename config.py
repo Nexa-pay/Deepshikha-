0af12385da3e55dc1946444b2d89c2d2cb1547a9
Nexa-pay <stellargamingg99@gmail.com>
2026-03-18 03:30:02 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,6 +4,5 @@ TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 MONGO_URI = os.getenv("MONGO_URI")
 
-RAZORPAY_KEY = os.getenv("RAZORPAY_KEY")
-RAZORPAY_SECRET = os.getenv("RAZORPAY_SECRET")
-WEBHOOK_SECRET = os.getenv("WEBHOOK_SECRET")
+OWNER_ID = int(os.getenv("OWNER_ID"))
+GROUP_ID = int(os.getenv("GROUP_ID"))
