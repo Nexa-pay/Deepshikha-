8ba4af2a2ccbcd08bf87628a89436e0a10116440
Nexa-pay <stellargamingg99@gmail.com>
2026-03-18 02:45:36 +0530

    Update voice.py

--- voice.py
+++ voice.py
@@ -1,1 +1,8 @@
+from gtts import gTTS
+import os
 
+def text_to_voice(text, user_id):
+    filename = f"voice_{user_id}.mp3"
+    tts = gTTS(text=text, lang='en')
+    tts.save(filename)
+    return filename
