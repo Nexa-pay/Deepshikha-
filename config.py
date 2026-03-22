f273ad8f20b8e996994ce98067f4e704cdc60441
Nexa-pay <stellargamingg99@gmail.com>
2026-03-22 13:42:33 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,29 +1,22 @@
 import os
 
 # ================= API =================
-OPENROUTER_API_KEY = os.getenv("OPENROUTER_API_KEY")
+TOGETHER_API_KEY = os.getenv("TOGETHER_API_KEY")
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 
 # ================= MODEL =================
-MODEL = "mistralai/mistral-7b-instruct"
+MODEL = "meta-llama/Llama-3-8b-chat-hf"
 
 TEMPERATURE = 0.7
 MAX_TOKENS = 60
-
-# ================= OWNER =================
-OWNER_NAME = "Aakash"
-OWNER_ID = 123456789
 
 # ================= DELAY =================
 MIN_DELAY = 2
 MAX_DELAY = 5
 
-# ================= IMAGE =================
-PHOTO_CHANCE = 40
-
 # ================= CHECK =================
-if not OPENROUTER_API_KEY:
-    raise ValueError("OPENROUTER_API_KEY missing")
+if not TOGETHER_API_KEY:
+    raise ValueError("TOGETHER_API_KEY missing")
 
 if not BOT_TOKEN:
     raise ValueError("BOT_TOKEN missing")
