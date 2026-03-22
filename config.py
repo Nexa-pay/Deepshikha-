d6fb99f2b3936882ba62dd2b45287ac5aeb42058
Nexa-pay <stellargamingg99@gmail.com>
2026-03-22 13:59:32 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,24 +1,16 @@
 import os
 
-# ================= API =================
 TOGETHER_API_KEY = os.getenv("TOGETHER_API_KEY")
+HF_API_KEY = os.getenv("HF_API_KEY")
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 
-# ================= MODEL =================
 MODEL = "meta-llama/Llama-3-8b-chat-hf"
+HF_MODEL = "HuggingFaceH4/zephyr-7b-beta"
 
 TEMPERATURE = 0.7
 MAX_TOKENS = 60
 
-# ================= DELAY =================
 MIN_DELAY = 2
 MAX_DELAY = 5
-
-# ================= CHECK =================
-if not TOGETHER_API_KEY:
-    raise ValueError("TOGETHER_API_KEY missing")
-
-if not BOT_TOKEN:
-    raise ValueError("BOT_TOKEN missing")
 
 print("✅ Config Loaded")
